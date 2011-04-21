4853cae79ccbdd3f56a414c9669e2dee098b4562
ngoote <ngoote@gmail.com>
2011-04-20 23:46:31 -0400

    Commiting partially completed dynamic programming solution.  Don't want to lose this, because it appears to be working.

--- PS8/ps8.py
+++ PS8/ps8.py
@@ -27,25 +27,16 @@ def loadSubjects(filename):
 
     subject_dict = {}
 
-    # The following sample code reads lines from the specified file and prints
-    # each one.
-    #inputFile = open(filename)
-    #for line in inputFile:
-    #    print (line)
-
-    # TODO: Instead of printing each line, modify the above to parse the name,
-    # value, and work of each subject and create a dictionary mapping the name
-    # to the (value, work).
-
     my_file = open(filename)
 
-    # Iterate over each line, taking action on each line.
+    # Iterate over each line, creating the dictionary as you go.
     for line in my_file.readlines():
         subject_name, subject_value, subject_work = line.split(',')
-        print (subject_name, subject_value, subject_work)
-        return
+        subject_dict[subject_name] = (int(subject_value), int(subject_work))
 
     my_file.close()
+
+    return subject_dict
 
 def printSubjects(subjects):
     """
@@ -112,7 +103,42 @@ def greedyAdvisor(subjects, maxWork, comparator):
     comparator: function taking two tuples and returning a bool
     returns: dictionary mapping subject name to (value, work)
     """
-    # TODO...
+    totalWorkSoFar = 0
+    solved_dict = {}
+    currentBestClass = "0.00"
+
+    # Loop while we are still finding valid classes to add
+    while totalWorkSoFar <= maxWork and currentBestClass != None:
+        currentBestClass = None
+
+        # Find the class with the most value that doesn't
+        # put us over the max work constraint
+        for key in subjects.keys():
+
+            # If we already have this class, skip it this time around
+            if key in solved_dict.keys():
+                continue
+
+            # If we haven't found a valid class yet, the only constraint is
+            # that it doesn't go over the maxWork constraint
+            if currentBestClass == None:
+
+                if totalWorkSoFar + subjects[key][WORK] <= maxWork:
+                    currentBestClass = key
+
+            # Otherwise we have found a valid class, and must determine
+            # if the current class is better per the comparator function
+            else:
+                # If it doesn't exceed maxWork and it's better than the
+                # current, make it the new current best
+                if totalWorkSoFar + subjects[key][WORK] <= maxWork and comparator(subjects[key], subjects[currentBestClass]):
+                    currentBestClass = key
+
+        if currentBestClass != None:
+            solved_dict[currentBestClass] = (subjects[currentBestClass][0], subjects[currentBestClass][1])
+            totalWorkSoFar = totalWorkSoFar + solved_dict[currentBestClass][WORK]
+
+    return solved_dict
 
 def bruteForceAdvisor(subjects, maxWork):
     """
@@ -124,8 +150,8 @@ def bruteForceAdvisor(subjects, maxWork):
     maxWork: int >= 0
     returns: dictionary mapping subject name to (value, work)
     """
-    nameList = subjects.keys()
-    tupleList = subjects.values()
+    nameList = list(subjects.keys())
+    tupleList = list(subjects.values())
     bestSubset, bestSubsetValue = \
             bruteForceAdvisorHelper(tupleList, maxWork, 0, None, None, [], 0, 0)
     outputSubjects = {}
@@ -165,12 +191,70 @@ def bruteForceTime():
     Runs tests on bruteForceAdvisor and measures the time required to compute
     an answer.
     """
-    # TODO...
+    subj_dict = loadSubjects(SUBJECT_FILENAME)
+
+    for i in range(1, 12):
+        start_time = time.time()
+
+        solved_dict = bruteForceAdvisor(subj_dict, i)
+
+        end_time = time.time()
+
+        print(solved_dict)
+
+        total_time = end_time - start_time
+
+        print("maxWork: %d" % i)
+        print("%0.2f seconds" % total_time)
+        print()
 
 # Problem 3 Observations
 # ======================
 #
-# TODO: write here your observations regarding bruteForceTime's performance
+#{'7.17': (10, 1)}
+#maxWork: 1
+#0.00 seconds
+#
+#{'6.00': (10, 1), '7.17': (10, 1)}
+#maxWork: 2
+#0.01 seconds
+#
+#{'6.00': (10, 1), '7.16': (7, 1), '7.17': (10, 1)}
+#maxWork: 3
+#0.03 seconds
+#
+#{'6.00': (10, 1), '7.16': (7, 1), '7.17': (10, 1), '12.04': (7, 1)}
+#maxWork: 4
+#0.09 seconds
+#
+#{'15.01': (7, 1), '6.00': (10, 1), '7.16': (7, 1), '7.17': (10, 1), '12.04': (7, 1)}
+#maxWork: 5
+#0.28 seconds
+#
+#{'7.16': (7, 1), '7.17': (10, 1), '7.00': (7, 1), '6.00': (10, 1), '12.04': (7, 1), '15.01': (7, 1)}
+#maxWork: 6
+#0.85 seconds
+#
+#{'7.16': (7, 1), '7.17': (10, 1), '7.00': (7, 1), '6.00': (10, 1), '24.12': (6, 1), '12.04': (7, 1), '15.01': (7, 1)}
+#maxWork: 7
+#2.47 seconds
+#
+#{'7.16': (7, 1), '7.17': (10, 1), '7.00': (7, 1), '6.00': (10, 1), '24.12': (6, 1), '12.04': (7, 1), '15.01': (7, 1), '2.03': (6, 1)}
+#maxWork: 8
+#7.03 seconds
+#
+#{'7.16': (7, 1), '7.17': (10, 1), '7.00': (7, 1), '6.00': (10, 1), '24.12': (6, 1), '7.18': (10, 2), '12.04': (7, 1), '15.01': (7, 1)}
+#maxWork: 9
+#18.64 seconds
+#
+#{'7.16': (7, 1), '7.17': (10, 1), '7.00': (7, 1), '6.00': (10, 1), '24.12': (6, 1), '7.18': (10, 2), '12.04': (7, 1), '15.01': (7, 1), '2.03': (6, 1)}
+#maxWork: 10
+#49.76 seconds
+#
+#{'7.16': (7, 1), '7.17': (10, 1), '7.00': (7, 1), '6.00': (10, 1), '24.12': (6, 1), '7.18': (10, 2), '12.04': (7, 1), '15.01': (7, 1), '22.03': (10, 2)}
+#maxWork: 11
+#125.49 seconds
+
 
 #
 # Problem 4: Subject Selection By Dynamic Programming
@@ -184,7 +268,44 @@ def dpAdvisor(subjects, maxWork):
     maxWork: int >= 0
     returns: dictionary mapping subject name to (value, work)
     """
-    # TODO...
+    classnames = list(subjects.keys())
+    print(classnames)
+    solutions = dpMax(subjects, classnames, len(subjects) - 1, maxWork)
+    print(str(solutions))
+
+def dpMax(subjects, classnames, i, availWork):
+    # If this is the last node, it must return either work or 0, because
+    # we can either take the class or not take the class
+    if i == 0:
+        if subjects[classnames[i]][WORK] <= availWork:
+            return (subjects[classnames[i]][VALUE], [classnames[i]])
+        else:
+            return (0, [])
+
+    without_i = dpMax(subjects, classnames, i - 1, availWork)
+
+    # If we don't have enough available work to include the current node,
+    # don't take that path
+    if subjects[classnames[i]][WORK] > availWork:
+        return without_i
+    else:
+        with_i    = dpMax(subjects, classnames, i - 1, availWork - subjects[classnames[i]][WORK])
+        with_i_value = with_i[0] + subjects[classnames[i]][VALUE]
+        with_i_bests = with_i[1]
+        with_i_bests.append(classnames[i])
+
+    if without_i[0] >= with_i_value:
+        print("without_i bigger")
+        print("without_i_value: ", str(without_i[0]), " without_i_bests: ", str(without_i[1]))
+        print("with_i_value: ", str(with_i_value), " with_i_bests: ", str(with_i_bests))
+        print()
+        return without_i
+    else:
+        print("with_i bigger")
+        print("without_i_value: ", str(without_i[0]), " without_i_bests: ", str(without_i[1]))
+        print("with_i_value: ", str(with_i_value), " with_i_bests: ", str(with_i_bests))
+        print()
+        return (with_i_value, with_i_bests)
 
 #
 # Problem 5: Performance Comparison
@@ -194,7 +315,8 @@ def dpTime():
     Runs tests on dpAdvisor and measures the time required to compute an
     answer.
     """
-    # TODO...
+    subj_dict = loadSubjects("test_input_dp.txt")
+    dpAdvisor(subj_dict, 17)
 
 # Problem 5 Observations
 # ======================
@@ -202,4 +324,7 @@ def dpTime():
 # TODO: write here your observations regarding dpAdvisor's performance and
 # how its performance compares to that of bruteForceAdvisor.
 
-loadSubjects(SUBJECT_FILENAME)
+if __name__ == '__main__':
+    #loadSubjects(SUBJECT_FILENAME)
+    #bruteForceTime()
+    dpTime()
